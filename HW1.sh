7f971a68df803bd80b6ddb3df90f15fc6043f412
Callie Steffen <calliesteffen@ESM.UCSB.EDU>
2019-10-18 15:42:21 -0700

    uploading changes to HW1

--- HW1.sh
+++ HW1.sh
@@ -8,17 +8,15 @@ set -x
 rasters="crefl2_A2019257204722-2019257205812_250m_ca-south-000_143"
 
 ogrinfo-al -so tl_2018_us_county/tl_2018_us_county.shp
-    # the shapefile that the rasters will be clipped to- We want santa barbara county 
-  
-roi=tl_2018_us_county.shp
+    # the shapefile that the rasters will be clipped to- We want santa barbara county. Projection/Attributes
+	
+	ogr2ogr -t_srs EPSG:3310\ -where "name='Santa Barbara'" aoi.shp tl_2018_us_county/tl_2018_us_county.shp 
+#this is theoretically projecting the vector to the NAD83/California Albers projection and taking the outline of SB county
 
-    # arguments used by all the GDAL commands we'll run
-    # -dstnotata -32768    : use -32768 as the output NO_DATA value
-    # -of GTiff            : make the output raster a GeoTIFF
-    # -co COMPRESS=DEFLATE : use the "DEFLATE" TIFF compression method
-    # -overwrite           : overwrite the output raster if it already exists
-    #
-common_args="-dstnodata -32768 -of GTiff -co COMPRESS=DEFLATE -overwrite"
+  
+roi=aoi.shp
+
+common_args="-dstalpha -of GTiff -co COMPRESS=DEFLATE -overwrite"
 
     # arguments for the clip command
     # -cutline $roi    : clip the input to the shapefile $roi
@@ -37,9 +35,9 @@ for file in $rasters
 do
         # clip ESRI grid InVEST_CV/$file into GeoTIFF ${file}_clipped.tif
         #
-    gdalwarp $common_args $clip_args InVEST_CV/$file ${file}_clipped.tif
+    gdalwarp $common_args $clip_args $file tl_2018_us_county/${file}_clipped.tif
 
         # project GeoTIFF ${file}_clipped.tif into GeoTIFF ${file}_utm10n.tif
         #
     gdalwarp $common_args $project_args ${file}_clipped.tif ${file}_utm10n.tif
-done
+done 
